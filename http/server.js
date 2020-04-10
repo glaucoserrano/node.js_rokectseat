12f7d5400f879abe0731ed0f447876cf4730130a
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-04-10 08:44:29 -0300

    server

---
+++ http/server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+http.createServer((req,res) => {
+    if(req.url === '/')
+        return res.end('<h1>Home Page</h1>')
+
+    if(req.url === '/contato')
+        return res.end('<h1>Contato</h1>')
+    
+    
+}).listen(5000,() => console.log("server is running"))
